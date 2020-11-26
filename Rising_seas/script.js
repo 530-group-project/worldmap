f5a9ae8801410ee4bea24357330a1a2042e1519d
Kang <70859744+newbofcode@users.noreply.github.com>
2020-11-26 13:58:22 -0500

    Add files via upload
    just added consistant.css and a folder for rising sea levels

---
+++ Rising_seas/script.js
@@ -0,0 +1,10 @@
+function openToc(){
+    document.getElementById('toc').style.marginLeft = '0';
+    document.getElementById('tocarrow').style.marginLeft = '230px';
+}
+
+function closeToc(){
+    document.getElementById('toc').style.marginLeft = '-250px';
+    document.getElementById('tocarrow').style.marginLeft = '-20px';
+
+}
